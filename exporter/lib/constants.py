ac57b1929fe7a4f94aec2bc41cc81ea0bfdb521a
Alex Thomae <git@alex.thom.ae>
2021-01-27 07:16:49 +0100

    Prepare release v1.7.13

--- exporter/lib/constants.py
+++ exporter/lib/constants.py
@@ -4,6 +4,6 @@
 
 WARN_TICKER_SLOW_LOAD = 'Exchange doesn\'t support fetching all the tickers in one go. Loading them individually.'
 
-VERSION = 'v1.7.11'
+VERSION = 'v1.7.13'
 # Gets set automatically at build time
 BUILD = 'dev'
